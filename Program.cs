ec733ae457d8d50a2936eb5f8655a7da7fa90f50
Dmitriy <dudin131@mail.ru>
2022-09-20 20:36:18 +0300

    Код отредактирован

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿string[] array1 = new string[5] { "hello", "2", "world", ":)", "dmitriy" };
-string[] array2 = new string[array1.Length];
+﻿string[] arrayFirst = new string[5] { "hello", "2", "world", ":)", "dmitriy" };
+string[] arraySecond = new string[arrayFirst.Length];
 int count = 0;
-for (int i = 0; i < array1.Length; i++)
+for (int i = 0; i < arrayFirst.Length; i++)
 {
-    if (array1[i].Length <= 3)
+    if (arrayFirst[i].Length <= 3)
     {
-        array2[count] = array1[i];
-        Console.Write($"{array2[count]} ");
+        arraySecond[count] = arrayFirst[i];
+        Console.Write($"{arraySecond[count]} ");
         count++;
     }
 }
