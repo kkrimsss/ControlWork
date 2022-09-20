d6515cd95e3b68c45dcd4f822b576ef1e21bb4bb
Dmitriy <dudin131@mail.ru>
2022-09-20 20:33:14 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array1 = new string[5] { "hello", "2", "world", ":)", "dmitriy" };
+string[] array2 = new string[array1.Length];
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        Console.Write($"{array2[count]} ");
+        count++;
+    }
+}
